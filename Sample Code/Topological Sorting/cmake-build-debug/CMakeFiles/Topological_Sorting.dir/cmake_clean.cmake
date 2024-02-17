dc19cafaf18286943c7051f675103fcd8d964793
Cuong Nguyen Le <nguyenlecuongaaa@gmail.com>
2024-02-17 04:31:58 +0300

    17.02.2024

---
+++ Sample Code/Topological Sorting/cmake-build-debug/CMakeFiles/Topological_Sorting.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Topological_Sorting.dir/main.cpp.obj"
+  "CMakeFiles/Topological_Sorting.dir/main.cpp.obj.d"
+  "Topological_Sorting.exe"
+  "Topological_Sorting.exe.manifest"
+  "Topological_Sorting.pdb"
+  "libTopological_Sorting.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Topological_Sorting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Sample Code/Topological Sorting/cmake-build-debug/CMakeFiles/Topological_Sorting.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Topological_Sorting.
+# This may be replaced when dependencies are built.
